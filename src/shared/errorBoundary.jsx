fc590395e630118572360bd843afb9be3ce64499
Emre Yigit Kuzhan <emreyigit.kuzhan@fibabanka.com.tr>
2020-06-02 10:23:38 +0300

    errorBounday updated

--- src/shared/errorBoundary.jsx
+++ src/shared/errorBoundary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logger from '../logger';
 // import logger from '../../logger';
 // import winston from 'winston';
 
@@ -16,6 +17,7 @@ class ErrorBoundary extends React.Component {
     //logErrorToMyService(error, info);
     // logger.log('Error', error);
     console.log('ComponentDidCatch', error, info);
+    logger.info(error);
   }
   render() {
     if (this.state.hasError) {
